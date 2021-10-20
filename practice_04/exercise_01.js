fa3df183c5333074c50c114f4f3df6213d3829b3
Antonio-Corleone <vuminhtuan3795@gmail.com>
2021-10-20 16:32:00 +0700

    Update

--- practice_04/exercise_01.js
+++ practice_04/exercise_01.js
@@ -1,8 +1,8 @@
-
+var txtEx011 = document.getElementById("resultEx011");
+var txtEx012 = document.getElementById("resultEx012");
 document.querySelector("#btn_ex011").onclick = function () {
     var soChan = "";
     var soLe = "";
-    var txtEx011 = document.getElementById("resultEx011");
     //Sử dụng vòng lặp for
     for (var i = 0; i <= 100; i++) {
         if (i % 2 == 0) {
@@ -16,13 +16,12 @@ document.querySelector("#btn_ex011").onclick = function () {
     txtEx011.innerHTML = soChan;
     txtEx012.innerHTML = soLe;
 }
-document.querySelector("#btn_ex012").onclick = function(){
+document.querySelector("#btn_ex012").onclick = function () {
     var soChan = "";
     var soLe = "";
-    var txtEx012 = document.getElementById("resultEx012");
     // Sử dụng vòng lặp while
     var i = 0;
-    while(i <= 100){
+    while (i <= 100) {
         if (i % 2 == 0) {
             soChan += " " + i;
         }
